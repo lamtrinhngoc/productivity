414eca299421506b2ec9e64704d88954dac3e5ca
lamtrinhngoc <lam.trinhngoc@shopee.com>
2025-03-26 11:09:11 +0700

    Update soc-linehaul.py

--- soc-linehaul.py
+++ soc-linehaul.py
@@ -43,25 +43,72 @@ def main():
     for col in date_columns:
         df_all_member_productivity[col] = pd.to_datetime(df_all_member_productivity[col], errors='coerce')
 
-    filter_datetime = df_all_member_productivity[
+    # File SOC & LineHaul Nationwide (c Hoa + c Hân)
+
+    soc_linehaul = df_all_member_productivity[
         (df_all_member_productivity['team'] == "Hoa Bui") |
         (df_all_member_productivity['team'] == "Gia Han") |
         (df_all_member_productivity['position'].str.contains("Driver", na=False))
     ]
 
     for col in date_columns:
-        filter_datetime[col] = filter_datetime[col].dt.strftime('%Y-%m-%d')
+        soc_linehaul[col] = soc_linehaul[col].dt.strftime('%Y-%m-%d')
 
-    filter_datetime = filter_datetime.replace({np.nan: '', np.inf: '', -np.inf: ''})
+    soc_linehaul = soc_linehaul.replace({np.nan: '', np.inf: '', -np.inf: ''})
 
     # Open the target spreadsheet and update with filtered data
-    nationwide_spreadsheet = open_spreadsheet_by_url('https://docs.google.com/spreadsheets/d/1zHEWFEwyZ6zq88hUvQfEzqwipCnHpdaovIl8hXFhPPw/edit?gid=714896083#gid=714896083')
-    if nationwide_spreadsheet is None:
+    soc_linehaul_spreadsheet = open_spreadsheet_by_url('https://docs.google.com/spreadsheets/d/1zHEWFEwyZ6zq88hUvQfEzqwipCnHpdaovIl8hXFhPPw/edit?gid=714896083#gid=714896083')
+    if soc_linehaul_spreadsheet is None:
         return
 
-    nationwide_sheet = nationwide_spreadsheet.worksheet("Raw Productivity")
-    nationwide_sheet.update(
-        [filter_datetime.columns.values.tolist()] + filter_datetime.values.tolist(),
+    soc_linehaul_sheet = soc_linehaul_spreadsheet.worksheet("Raw Productivity")
+    soc_linehaul_sheet.update(
+        [soc_linehaul.columns.values.tolist()] + soc_linehaul.values.tolist(),
+        value_input_option='USER_ENTERED'
+    )
+
+    # File BD Projection (c Hân)
+
+    binh_duong = df_all_member_productivity[
+        (df_all_member_productivity['team'] == "Gia Han") |
+        (df_all_member_productivity['station_name'].str.contains("Binh Duong 1 SOC", na=False))
+    ]
+
+    for col in date_columns:
+        binh_duong[col] = binh_duong[col].dt.strftime('%Y-%m-%d')
+
+    binh_duong = binh_duong.replace({np.nan: '', np.inf: '', -np.inf: ''})
+
+    # Open the target spreadsheet and update with filtered data
+    binh_duong_spreadsheet = open_spreadsheet_by_url('https://docs.google.com/spreadsheets/d/1A7hwKMN74dMCFUJ_QF1cGy41FdGAOQhga19l_I3O01Y/edit?gid=0#gid=0')
+    if binh_duong_spreadsheet is None:
+        return
+
+    binh_duong_sheet = binh_duong_spreadsheet.worksheet("Raw Productivity'")
+    binh_duong_sheet.update(
+        [binh_duong.columns.values.tolist()] + binh_duong.values.tolist(),
+        value_input_option='USER_ENTERED'
+    )
+
+    # File Nationwide Scheme Efficiency
+
+    two_months_ago = pd.Timestamp.today().replace(day=1) - pd.DateOffset(months=2)
+
+    scheme_efficiency = df_all_member_productivity[df_all_member_productivity['date_update'] >= two_months_ago]
+
+    for col in date_columns:
+        scheme_efficiency[col] = scheme_efficiency[col].dt.strftime('%Y-%m-%d')
+
+    scheme_efficiency = scheme_efficiency.replace({np.nan: '', np.inf: '', -np.inf: ''})
+
+    # Open the target spreadsheet and update with filtered data
+    scheme_efficiency_spreadsheet = open_spreadsheet_by_url('https://docs.google.com/spreadsheets/d/1ZghVwm_7cniD1CdFNwXz9gOjoggUqzxxznqT0hTDLk0/edit?gid=0#gid=0')
+    if scheme_efficiency_spreadsheet is None:
+        return
+
+    scheme_efficiency_sheet = scheme_efficiency_spreadsheet.worksheet("Raw Productivity")
+    scheme_efficiency_sheet.update(
+        [scheme_efficiency.columns.values.tolist()] + scheme_efficiency.values.tolist(),
         value_input_option='USER_ENTERED'
     )
 

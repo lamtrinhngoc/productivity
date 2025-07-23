65e11627f4315dc7f84443d9f405c9d044522b00
lamtrinhngoc <lam.trinhngoc@shopee.com>
2025-07-23 17:35:56 +0700

    Update productivity.py

--- productivity.py
+++ productivity.py
@@ -37,7 +37,7 @@ class GSpreadClientWithCache:
 )
 def read_worksheet_with_retry(sheet, sheet_name, schema):
     worksheet = sheet.worksheet(sheet_name)
-    data = worksheet. worksheet.get('B8:AP') 
+    data = worksheet.get('B8:AP') 
     if not data:
         return pd.DataFrame(columns=schema)
     df = pd.DataFrame(data)
